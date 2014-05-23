4b04828ee4ba2a288dc4a7243c3104078db360dd
jonathan robinson <jonbrobinson@gmail.com>
2014-05-23 10:57:13 -0500

    Added second form to show textarea and different input types

--- public/my_first_form.php
+++ public/my_first_form.php
@@ -22,8 +22,29 @@
 	        <input id="password" name="password" type="password" placeholder="Username">
 	    </p>
 	    <p>
-	        <button type="submit" >Login</button>
+	        <button type="submit">Login</button>
 	        <button type="reset">Reset</button>
+	    </p>
+	</form>
+	<form method="POST">
+		<h3>Send an Email</h3>
+		<p>
+			<label for="email_to">To</label>
+			<input id="email_to" name="EMto" type="email" placeholder="Recepiant">
+		</p>
+		<p>
+			<label for="email_from">From</label>
+			<input id="email_from" name="EMfrom" type="email" placeholder="Sender">
+		</p>
+		<p>
+			<label for="email_subject">Subject</label>
+			<input id="email_subject" name="EMsubject" type="text" placeholder="Subject">
+		</p>
+		<p>
+			<textarea id="email_subject" name="EMbody" rows="5" cols="30" placeholder="Enter Content"></textarea>
+		</p>
+		<p>
+	        <button type="submit">Send</button>
 	    </p>
 	</form>
 
